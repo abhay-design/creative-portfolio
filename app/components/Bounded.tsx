bc654452fe2d673cbfa51443de4d6f65c246e9fd
Abhay <abhaydec48@gmail.com>
2025-06-10 12:32:18 +0530

    added hero slice

---
+++ app/components/Bounded.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import clsx from "clsx";
+
+type BoundedProps<T extends React.ElementType = "section"> = {
+  as?: T;
+  className?: string;
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<T>;
+
+const Bounded = React.forwardRef<HTMLElement, BoundedProps>(
+  ({ as: Comp = "section", className, children, ...restProps }, ref) => {
+    return (
+      <Comp
+        ref={ref}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+        {...restProps}
+      >
+        <div className="mx-auto w-full max-w-7xl">{children}</div>
+      </Comp>
+    );
+  }
+);
+
+Bounded.displayName = "Bounded";
+
+export default Bounded;
